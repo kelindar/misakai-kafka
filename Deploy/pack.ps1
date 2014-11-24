cb6b395d4cb68aa259d9e5f04d049f606afd326b
Roman <roman.atachiants@gmail.com>
2014-11-24 23:23:14 +0000

    nuget integration test

---
+++ Deploy/pack.ps1
@@ -0,0 +1,12 @@
+$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\Misakai.Kafka\bin\Release\Misakai.Kafka.dll").GetName().Version
+$versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
+
+Write-Host "Setting .nuspec version tag to $versionStr"
+
+$content = (Get-Content $root\Deploy\Misakai.Kafka.nuspec) 
+$content = $content -replace '\$version\$',$versionStr
+
+$content | Out-File $root\nuget\Misakai.Kafka.compiled.nuspec
+
+& $root\Deploy\NuGet.exe pack $root\nuget\Misakai.Kafka.compiled.nuspec
